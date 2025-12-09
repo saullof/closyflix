9d02e3050ef96e5660fa88463f5d8959f6121697
Saullof <agenciavalet@gmail.com>
2025-12-09 16:55:35 -0300

    Load user pixels on checkout pages

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Model\Coupon;
+use App\Model\UserPixel;
 
 class CheckoutController extends Controller
 {
@@ -16,13 +17,22 @@ class CheckoutController extends Controller
     {
         $user = User::where('username', $username)->firstOrFail();
         $coupon = null;
-        
+        $pixel_user = [];
+
         if ($coupon_code) {
             $coupon = Coupon::where('coupon_code', $coupon_code)
                 ->where('creator_id', $user->id)
                 ->where('status', 'active')
                 ->first();
         }
-        return view('pages.checkout', compact('user', 'coupon'));
+
+        $pixels = UserPixel::where('user_id', $user->id)->get();
+
+        foreach ($pixels as $pixel) {
+            $pixel_user[$pixel->type . "-head"] = $pixel->head;
+            $pixel_user[$pixel->type . "-body"] = $pixel->body;
+        }
+
+        return view('pages.checkout', compact('user', 'coupon', 'pixel_user'));
     }
 }
